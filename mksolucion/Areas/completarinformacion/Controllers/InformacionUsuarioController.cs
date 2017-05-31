c578751d033fd8df58c7e81dee044e557708a1ee
arongonzo <arongonzo@gmail.com>
2017-05-31 17:00:01 -0400

    primer ingreso actualizacion 1

--- mksolucion/Areas/completarinformacion/Controllers/InformacionUsuarioController.cs
+++ mksolucion/Areas/completarinformacion/Controllers/InformacionUsuarioController.cs
@@ -56,8 +56,8 @@ namespace mksolucion.Areas.completarinformacion.Controllers
                     db.SaveChanges();
                     stx_tipo = cnt02_tipocuenta.cnt02_id;
                 }
-
-                decimal stx_cliente = 0;
+                decimal llave_servicio = 0;
+                decimal llave_cliente = 0;
                 var queryCliente = from usr in db.AspNetUsers
                                    join cntusr in db.cnt03_cuenta_usuario on usr.Id equals cntusr.UserId
                                    join cnt in db.cnt01_cuenta on cntusr.cnt01_id equals cnt.cnt01_id
@@ -71,7 +71,7 @@ namespace mksolucion.Areas.completarinformacion.Controllers
                     var datoscliente = queryCliente.ToList();
                     foreach (var RowC in datoscliente)
                     {
-                        stx_cliente = RowC.cnt01_id;
+                        llave_cliente = RowC.cnt01_id;
                     }
                 }
                 else
@@ -85,15 +85,141 @@ namespace mksolucion.Areas.completarinformacion.Controllers
 
                     db.cnt01_cuenta.Add(cnt01_cuenta);
                     db.SaveChanges();
-                    stx_cliente = cnt01_cuenta.cnt01_id;
+                    llave_cliente = cnt01_cuenta.cnt01_id;
 
-                    cnt03_cuenta_usuario cnt03_cuenta_usuario =  new cnt03_cuenta_usuario()
-                    cnt03_cuenta_usuario.cnt01_id = stx_cliente;
+                    cnt03_cuenta_usuario cnt03_cuenta_usuario = new cnt03_cuenta_usuario();
+                    cnt03_cuenta_usuario.cnt01_id = llave_cliente;
                     cnt03_cuenta_usuario.UserId = llave_usario;
 
                     db.cnt03_cuenta_usuario.Add(cnt03_cuenta_usuario);
                     db.SaveChanges();
                 }
+
+                decimal llave_plan = 0, llave_tipoplan = 0, llave_cobro = 0;
+                var queryplanes = from pln in db.pln01_planes
+                                  join tppln in db.pln02_tipoplan on pln.pln02_id equals tppln.pln02_id
+                                  join tpcbr in db.pln03_tipocobro on tppln.pln03_id equals tpcbr.pln03_id
+                                  where tpcbr.pln03_nombre == "Gratis"
+                                  || tppln.pln02_nombre == "Invitado"
+                                  || pln.pln01_nombre == "Plan Inicial"
+
+                            select new
+                            {
+                                pln01_id = pln.pln01_id,
+                                pln02_id = tppln.pln02_id,
+                                pln03_id = tpcbr.pln03_id
+                            };
+                if (queryplanes.Count() > 0)
+                {
+                    var datosplanes = queryplanes.ToList();
+                    foreach (var Rowplanes in datosplanes)
+                    {
+                        llave_plan = (decimal)Rowplanes.pln01_id;
+                        llave_tipoplan = (decimal)Rowplanes.pln02_id;
+                        llave_cobro = (decimal)Rowplanes.pln03_id;
+                    }
+                }
+                else
+                {
+                    var querycobro = from tpcbr in db.pln03_tipocobro 
+                                      where tpcbr.pln03_nombre == "Gratis"
+                                      
+                                      select new
+                                      {
+                                          pln03_id = tpcbr.pln03_id
+                                      };
+                    if (querycobro.Count() > 0)
+                    {
+                        var datoscobro = querycobro.ToList();
+                        foreach (var Rowcobro in datoscobro)
+                        {
+                            llave_cobro = (decimal)Rowcobro.pln03_id;
+                        }
+                    }
+                    else
+                    {
+                        pln03_tipocobro pln03_tipocobro = new pln03_tipocobro();
+                        pln03_tipocobro.pln03_nombre = "Gratis";
+                        pln03_tipocobro.pln03_descripcion = "Sin cobro para el cliente";
+                        pln03_tipocobro.pln03_estado = 1;
+                        pln03_tipocobro.pln03_fechacreacion = DateTime.Now;
+                        pln03_tipocobro.pln03_ultimaactualizacion = DateTime.Now;
+                        db.pln03_tipocobro.Add(pln03_tipocobro);
+                        db.SaveChanges();
+                        llave_cobro = (decimal)pln03_tipocobro.pln03_id;
+                    }
+
+
+                    var querytipoplan = from tppln in db.pln02_tipoplan
+                                        where tppln.pln02_nombre == "Invitado"
+
+                                        select new
+                                        {
+                                            pln02_id = tppln.pln02_id
+                                        };
+
+                    if (querytipoplan.Count() > 0)
+                    {
+                        var datostipoplan = querytipoplan.ToList();
+                        foreach (var Rowtipoplan in datostipoplan)
+                        {
+                            llave_tipoplan = (decimal)Rowtipoplan.pln02_id;
+                        }
+                    }
+                    else
+                    {
+                        pln02_tipoplan pln02_tipoplan = new pln02_tipoplan();
+                        pln02_tipoplan.pln02_nombre = "Invitado";
+                        pln02_tipoplan.pln02_descripcion = "para usuarios que se han registrado en el sistema";
+                        pln02_tipoplan.pln03_id = llave_cobro;
+                        pln02_tipoplan.pln02_estado = 1;
+                        pln02_tipoplan.pln02_fechacreacion = DateTime.Now;
+                        pln02_tipoplan.pln02_ultimaactualizacion = DateTime.Now;
+                        db.pln02_tipoplan.Add(pln02_tipoplan);
+                        db.SaveChanges();
+                        llave_tipoplan = (decimal)pln02_tipoplan.pln02_id;
+                    }
+
+                    var queryplan = from pln in db.pln01_planes
+                                    where pln.pln01_nombre == "Plan Inicial"
+
+                                        select new
+                                        {
+                                            pln01_id = pln.pln01_id
+                                        };
+                    if (queryplan.Count() > 0)
+                    {
+                        var datosplan = queryplan.ToList();
+                        foreach (var Rowplan in datosplan)
+                        {
+                            llave_plan = (decimal)Rowplan.pln01_id;
+                        }
+                    }
+                    else
+                    {
+                        pln01_planes pln01_planes = new pln01_planes();
+                        pln01_planes.pln01_nombre = "Plan Inicial";
+                        pln01_planes.pln01_detalle = "Plan inicial";
+                        pln01_planes.pln02_id = (decimal)llave_tipoplan;
+                        pln01_planes.pln01_activo = 1;
+                        pln01_planes.pln01_fechacreacion = DateTime.Now;
+                        pln01_planes.pln01_ultimaactualizacion = DateTime.Now;
+                        db.pln01_planes.Add(pln01_planes);
+                        db.SaveChanges();
+                        llave_plan = (decimal)pln01_planes.pln01_id;
+                    }
+
+                }
+
+                serv01_servicios serv01_servicios = new serv01_servicios();
+                serv01_servicios.cnt01_id = llave_cliente;
+                serv01_servicios.pnl01_id = llave_plan;
+                serv01_servicios.ser01_estado = 1;
+                serv01_servicios.ser01_fechacreacion = DateTime.Now;
+                serv01_servicios.ser01_ultimaactualizacion = DateTime.Now;
+                db.serv01_servicios.Add(serv01_servicios);
+                db.SaveChanges();
+                llave_servicio = (decimal)serv01_servicios.ser01_id;
 
 
 
--- mksolucion/Areas/portal/Models/serv01_servicios.cs
+++ mksolucion/Areas/portal/Models/serv01_servicios.cs
@@ -9,21 +9,35 @@ namespace mksolucion.Models
 
     public partial class serv01_servicios
     {
+        [ScaffoldColumn(false)]
         [Key]
         [Column(TypeName = "numeric")]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Servicio")]
         public decimal ser01_id { get; set; }
 
+        [Display(Name = "Cuenta de Usuario")]
         [Column(TypeName = "numeric")]
         public decimal? cnt01_id { get; set; }
 
+        [Display(Name = "Plan")]
         [Column(TypeName = "numeric")]
         public decimal? pnl01_id { get; set; }
 
+        [ScaffoldColumn(false)]
+        [Display(Name = "Estado")]
         public int? ser01_estado { get; set; }
 
+        [ScaffoldColumn(false)]
+        [Display(Name = "Fecha creación")]
+        [DisplayFormat(DataFormatString = "{0:dd-mm-yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
         public DateTime? ser01_fechacreacion { get; set; }
 
+        [Display(Name = "Fecha Actualización")]
+        [DisplayFormat(DataFormatString = "{0:dd-mm-yyyy}", ApplyFormatInEditMode = true)]
+        [ScaffoldColumn(false)]
+        [DataType(DataType.Date)]
         public DateTime? ser01_ultimaactualizacion { get; set; }
 
         public virtual cnt01_cuenta cnt01_cuenta { get; set; }
